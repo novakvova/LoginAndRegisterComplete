a8b0054b7615d1e7281951f520e73ad5e794893e
novakvova <novakvova@gmail.com>
2018-11-20 18:36:16 +0200

    Begin post form

--- src/components/signup/SignupForm.js
+++ src/components/signup/SignupForm.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import timezone from '../../data/timezones';
+import map from 'lodash/map';
+import timezones from '../../data/timezones';
+import axios from 'axios';
 
 class SignupForm extends React.Component {
     constructor(props) {
@@ -11,12 +14,87 @@ class SignupForm extends React.Component {
             passwordConfirmation: '',
             timezone: ''
         };
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
 
     }
+    onChange(e) {
+        this.setState({ [e.target.name]: e.target.value});
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+        axios.post('api/register', this.state);
+        console.log(this.state);
+    }
     render() {
+        const options = map(timezones, (val, key) =>
+            <option key={val} value={val}>{key}</option>
+        );
         return (
-            <form>
+            <form onSubmit={this.onSubmit}>
                 <h1>Join our community!</h1>
+                <div className="form-group">
+                    <label className="control-label">Username</label>
+                    <input
+                        value={this.state.username}
+                        onChange={this.onChange}
+                        type="text"
+                        name="username"
+                        className="form-control"
+                    />
+                </div>
+
+                 <div className="form-group">
+                    <label className="control-label">Email</label>
+                    <input
+                        value={this.state.email}
+                        onChange={this.onChange}
+                        type="text"
+                        name="email"
+                        className="form-control"
+                    />
+                </div>
+                
+                <div className="form-group">
+                    <label className="control-label">Password</label>
+                    <input
+                        value={this.state.password}
+                        onChange={this.onChange}
+                        type="password"
+                        name="password"
+                        className="form-control"
+                    />
+                </div>
+
+                <div className="form-group">
+                    <label className="control-label">Password Confirmation</label>
+                    <input
+                        value={this.state.passwordConfirmation}
+                        onChange={this.onChange}
+                        type="password"
+                        name="passwordConfirmation"
+                        className="form-control"
+                    />
+                </div>
+
+                <div className="form-group">
+                    <label className="control-label">Timezone</label>
+                    <select
+                        value={this.state.timezone}
+                        onChange={this.onChange}
+                        name="timezone"
+                        className="form-control">
+                        <option value="" disabled>Choose Yout Timezone</option>
+                        {options}
+                    </select>
+                </div>
+
+                <div className="form-group">
+                    <button className="btn btn-primary btn-lg">
+                        Sign up
+                    </button>
+                </div>
             </form>
         );
     }
